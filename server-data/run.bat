2fc9fc286ad307d2816f0048f15fcca8bc1d9e29
Time_XP <alexandremaji@gmail.com>
2022-11-28 19:40:05 +0000

    :pencil2: | Fix command line in run script.

--- server-data/run.bat
+++ server-data/run.bat
@@ -1,2 +1,2 @@
-cd <path_to_your_server>\txp_fxserver_vanilla_template\server-data
-<path_to_your_server>\txp_fxserver_vanilla_template\server\FXServer.exe +exec server.cfg
+cd <path_to_your_server>\txp_fxserver_vanilla_template_windows\server-data
+<path_to_your_server>\txp_fxserver_vanilla_template_windows\server\FXServer.exe +exec server.cfg
